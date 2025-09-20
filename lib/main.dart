e351b409bb1ad5cfe5a3990d4ffeec0eeedd7123
José Ortiz Hernández <joseantonioortizhernandez777@gmail.com>
2025-09-20 17:50:54 -0600

    feat(auth): login & register UI + navegación desde Home

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'src/core/theme/app_theme.dart';
 import 'src/features/home/presentation/pages/home_page.dart';
+// 👇 importa la nueva pantalla
+import 'src/features/auth/presentation/pages/login_page.dart';
+import 'src/features/auth/presentation/pages/register_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,10 +18,17 @@ class AuraApp extends StatelessWidget {
     return MaterialApp(
       title: 'Aura',
       debugShowCheckedModeBanner: false,
-
       theme: AppTheme.light,
+
+      // 👇 muestra Login primero
       home: const HomePage(),
 
+      // (opcional) deja registrada Home por si la navegas luego
+      routes: {
+        '/home': (_) => const HomePage(),
+        LoginPage.routeName: (_) => const LoginPage(),
+        RegisterPage.routeName: (_) => const RegisterPage(),
+      },
     );
   }
 }
